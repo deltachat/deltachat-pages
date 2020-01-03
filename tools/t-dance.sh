3a5ddbc9fc7b324bc8da0e8e3895329088aa2f67
lefherz <emil.lefherz@fau.de>
2020-01-03 17:52:30 +0100

    changes to t-dance.sh

--- tools/t-dance.sh
+++ tools/t-dance.sh
@@ -17,14 +17,18 @@
 # common information about the Transifex CLI client can be found at:
 # https://docs.transifex.com/client/
 
+set -e
 
-sfiles=(blog contribute download gdpr help imprint index references verify_downloads)
+sfiles=(blog contribute download gdpr help imprint index references verify-downloads)
 tlangs=(ca de es fr it nb_NO pl pt ru sq uk)  # do not add `en` to this list
 
 
 pull_po_translations_from_tx() {
-	rm -r translations
-	tx pull -a   # -a = fetch all translationss, -s = fetches source
+	find ../_data/lang/ -type f -not -name 'en.*' -delete
+	rm -r translations || true
+	mkdir translations
+ 	ln -s -T ../../_data/lang translations/delta-chat-pages.yml
+	tx pull -a --mode=sourcetranslation  # -a = fetch all translationss, -s = fetches source
 	for sfile in ${sfiles[@]}; do
 		for tlang in ${tlangs[@]}; do
 			pofile="../${tlang:0:2}/${sfile}.po"
@@ -35,6 +39,7 @@ pull_po_translations_from_tx() {
 
 
 push_po_sources_to_tx() {
+ 	ln -s -T ../../_data/lang translations/delta-chat-pages.yml || true
 	tx push -s
 }
 
