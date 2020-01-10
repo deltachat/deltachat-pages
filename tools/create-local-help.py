36d8d9dea5f4f8657db1982b6be23dc11b21997f
B. Petersen <r10s@b44t.com>
2020-01-10 23:26:21 +0100

    add python script for generating local-help

--- tools/create-local-help.py
+++ tools/create-local-help.py
@@ -6,16 +6,44 @@ import os
 import re
 
 
+def read_file(filename):
+    f = open(filename, 'r')
+    content = f.read()
+    f.close()
+    return content
+
+
+def write_file(filename, content):
+    f = open(filename, 'w')
+    f.write(content)
+    f.close()
+
+
 def generate_file(destdir, lang, file):
     print("generate local help in " + destdir + "/" + lang + "/" + file)
 
-    f = open("../_site/" + lang + "/" + file + ".html", 'r')
-    content = f.read()
-    f.close()
+    content = read_file("../_site/" + lang + "/" + file + ".html")
 
-    f = open(destdir + "/" + lang + "/" + file + ".html", 'w')
-    f.write(content)
-    f.close()
+    content = re.sub(r"^.*<div id=\"content\">.*<h1>.*?</h1>.*?<ul.*?>",
+                       "<!DOCTYPE html>\n"
+                     + "<html>"
+                     +   "<head>"
+                     +     "<meta charset=\"UTF-8\" />"
+                     +     "<link rel=\"stylesheet\" href=\"../help.css\" />"
+                     +   "</head>"
+                     +   "<body>"
+                     +     "<ul id=\"top\">",
+                     content,
+                     flags=re.MULTILINE|re.DOTALL)
+
+    content = re.sub(r"</div>.*?</body>.*</html>.*$",
+                         "</body>"
+                     + "</html>",
+                     content,
+                     flags=re.MULTILINE|re.DOTALL)
+
+    write_file(destdir + "/" + lang + "/" + file + ".html", content)
+
 
 def generate_lang(destdir, lang):
     generate_file(destdir, lang, "help")
