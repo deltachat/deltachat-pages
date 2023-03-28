e69e512e12056884ff45aafc998fe3dd2428f262
B. Petersen <r10s@b44t.com>
2023-03-28 18:21:15 +0200

    instead of passing --all to the tx command, create the files we're interested in; this is more than twice faster

--- tools/t-dance.sh
+++ tools/t-dance.sh
@@ -25,10 +25,21 @@ tlangs=(ca cs de es fr gl id it nl pl pt ru sk sq tr uk zh_CN)  # do not add `en
 
 
 pull_po_translations_from_tx() {
-	#find ../_data/lang/ -type f -not -name 'en.*' -delete
 	rm -r translations || true
-	mkdir translations 
-	tx pull -f
+
+	# instead of passing --all to the tx command, create the files we're interested in; this is more than twice faster
+	mkdir "translations"
+	mkdir "translations/delta-chat-pages.yml"
+	for sfile in ${sfiles[@]}; do
+	  mkdir "translations/delta-chat-pages.${sfile}po"
+	  for tlang in ${tlangs[@]}; do
+	    touch "translations/delta-chat-pages.yml/${tlang}.yml"
+	    touch "translations/delta-chat-pages.${sfile}po/${tlang}.po"
+	  done
+	done
+
+	tx pull --translations --force
+
 	for tlang in ${tlangs[@]}; do
 	  echo "Converting ${tlang} ..."
 	  for sfile in ${sfiles[@]}; do
