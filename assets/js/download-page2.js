13d6e74cd65aff42f77a4a00dc180818e574e3fc
B. Petersen <r10s@b44t.com>
2023-07-02 17:44:52 +0200

    typo

--- assets/js/download-page2.js
+++ assets/js/download-page2.js
@@ -13,7 +13,7 @@ function userAgentToOS() {
     // return the OS, if in doubt, an empty string is returned
     var uA = navigator.userAgent;
     if (uA.includes('Ubuntu; Mobile') || uA.includes('Ubuntu; Tablet') || (uA.includes('Linux; Ubuntu') && uA.includes('like Android'))) {
-        // first two are from https://daker.me/2013/11/ubuntu-touch-user-agent.html, third one if from testing with current ubuntu touch device
+        // first two are from https://daker.me/2013/11/ubuntu-touch-user-agent.html, third one is from testing with recent ubuntu touch device
         return 'ubuntutouch';
     } else if (uA.includes('iPad') && uA.includes('Mobile'))
         return 'ios';
