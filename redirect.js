e6b5a03d1765ad27f04850334cb945279a0cf783
B. Petersen <r10s@b44t.com>
2026-08-18 13:58:58 +0200

    add farsi redirection

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 
 function getLanguageFolder() {
-  var supported = ['id', 'ca', 'cs', 'de', 'es', 'eu', 'fr', 'gl', 'it', 'nl',
+  var supported = ['id', 'ca', 'cs', 'de', 'es', 'eu', 'fa', 'fr', 'gl', 'it', 'nl',
                    'pl', 'pt', 'pt_BR', 'sq', 'sk', 'tr', 'ru', 'uk', 'zh_CN'];
 
   var desired = navigator.language || navigator.userLanguage;
